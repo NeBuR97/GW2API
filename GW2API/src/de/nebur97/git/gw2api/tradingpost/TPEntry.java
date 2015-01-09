080ff00decbe726e6eba7c1418fdc87bf8219bd8
NeBuR Shizam <neburshizam@gmail.com>
2015-01-10 00:39:50 +0100

    an entry in the Trading Post

--- GW2API/src/de/nebur97/git/gw2api/tradingpost/TPEntry.java
+++ GW2API/src/de/nebur97/git/gw2api/tradingpost/TPEntry.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import de.nebur97.git.gw2api.item.Item;
 
-<<<<<<< HEAD
 public class TPEntry implements Serializable
 {
     /**
@@ -94,70 +93,4 @@ public class TPEntry implements Serializable
     {
 	return "id:" + id + ",buy:" + buy + ",sell:" + sell + ",demand:" + demand + ",supply:" + supply;
     }
-=======
-public class TPEntry implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2237053978430581438L;
-	private transient Item item;
-	private int id;
-	private int buy;
-	private int sell;
-	private int supply;
-	private int demand;
-	
-	public Item getItem() {
-		return item;
-	}
-	public void setItem(Item item) {
-		this.item = item;
-	}
-	public int getID() {
-		return id;
-	}
-	public void setID(int id) {
-		this.id = id;
-	}
-	public int getBuy() {
-		return buy;
-	}
-	public void setBuy(int buy) {
-		this.buy = buy;
-	}
-	public int getSell() {
-		return sell;
-	}
-	public void setSell(int sell) {
-		this.sell = sell;
-	}
-	public int getSupply() {
-		return supply;
-	}
-	public void setSupply(int supply) {
-		this.supply = supply;
-	}
-	public int getDemand() {
-		return demand;
-	}
-	public void setDemand(int demand) {
-		this.demand = demand;
-	}
-	
-	public String getBuyPriceInGW2Format()
-	{
-		
-        return TradingPost.copperValueToGW2StandardCurrencyString(buy);
-	}
-	
-	public String getSellPriceInGW2Format()
-	{
-		return TradingPost.copperValueToGW2StandardCurrencyString(sell);
-	}
-	@Override
-	public String toString()
-	{
-		return "id:"+id+",buy:"+buy+",sell:"+sell+",demand:"+demand+",supply:"+supply;
-	}
->>>>>>> origin/master
 }
